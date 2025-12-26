777d5449e8a7e8221b933a577809a3cdfe1a12bd
ANSHUMAN  MAURYA <anshumanmaurya776@gmail.com>
2025-12-26 23:27:40 +0530

    Update main.py

--- Anshuman_t_shirts_store/main.py
+++ Anshuman_t_shirts_store/main.py
@@ -3,11 +3,10 @@ from langchain_helper import get_few_shot_db_chain
 
 st.title("Anshuman T-Shirts Store 👕")
 
-question = st.text_input("Ask a question about the database:")
+question = st.text_input("Ask a question about the database")
 
 if question:
     chain = get_few_shot_db_chain()
-    response = chain.run(question)
+    response = chain.invoke({"input": question})
+    st.write(response["result"])
 
-    st.subheader("Answer")
-    st.write(response)
