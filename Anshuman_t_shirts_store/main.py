5bb960bb77df11dd0e009e55bfea7a7ee20f3433
ANSHUMAN  MAURYA <anshumanmaurya776@gmail.com>
2025-12-27 14:19:20 +0530

    Update main.py

--- Anshuman_t_shirts_store/main.py
+++ Anshuman_t_shirts_store/main.py
@@ -1,5 +1,6 @@
 import os
 import streamlit as st
+from langchain.chat_models import ChatOpenAI
 from langchain_helper import get_few_shot_db_chain
 
 st.title("Anshuman T-Shirts Store 👕")
@@ -8,7 +9,18 @@ question = st.text_input("Ask a question about the database")
 
 if question:
     chain = get_few_shot_db_chain()
+
+    # Run SQL query
     raw_result = chain.run(question)
 
     st.subheader("Answer")
-    st.write(raw_result[0][0])
+
+    # SAFE result handling
+    if isinstance(raw_result, list) and len(raw_result) > 0:
+        value = raw_result[0][0]
+        if value is None:
+            st.write(0)
+        else:
+            st.write(int(value))
+    else:
+        st.write(0)
