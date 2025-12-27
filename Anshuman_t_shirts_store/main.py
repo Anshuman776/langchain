9b464dc0abcd8ae0e2cff21a3c20b8aab16496f2
ANSHUMAN  MAURYA <anshumanmaurya776@gmail.com>
2025-12-27 14:04:31 +0530

    Update main.py

--- Anshuman_t_shirts_store/main.py
+++ Anshuman_t_shirts_store/main.py
@@ -1,6 +1,5 @@
 import os
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
 from langchain_helper import get_few_shot_db_chain
 
 st.title("Anshuman T-Shirts Store 👕")
@@ -11,24 +10,5 @@ if question:
     chain = get_few_shot_db_chain()
     raw_result = chain.run(question)
 
-    llm = ChatOpenAI(
-        model_name="gpt-3.5-turbo",
-        temperature=0.1,
-        openai_api_key=os.environ["OPENAI_API_KEY"]
-    )
-
-    explanation_prompt = f"""
-User question:
-{question}
-
-Database output:
-{raw_result}
-
-Explain the result clearly in plain English.
-Do NOT show SQL.
-"""
-
-    final_answer = llm.predict(explanation_prompt)
-
     st.subheader("Answer")
-    st.write(final_answer)
+    st.write(raw_result)
